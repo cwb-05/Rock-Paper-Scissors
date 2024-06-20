f2b7b7b54bd8b4a3819c8e69b1eba273fa9c3622
cwb-05 bass <Coyotekid@t-online.de>
2024-06-20 21:21:57 +0000

    Add const declarations

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+/**Declaring const elements */
+const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+const lossScore = document.getElementById('loss-score');
+const winScore = document.getElementById('win-score');
+const tieScore = document.getElementById('tie-score');
+const userImage = document.getElementById('user-img');
+const computerImage = document.getElementById('computer-img');
