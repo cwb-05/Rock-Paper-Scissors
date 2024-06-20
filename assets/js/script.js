8c5b85049e9136b4728f1d699fe2451e4da37bff
cwb-05 bass <Coyotekid@t-online.de>
2024-06-20 23:12:52 +0000

    Add main game funtion

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,21 @@ for (let button of buttons) {
 /**Main function. Accepts "data-coice" parameter selected by the user
  * through the buttons
  */
+function playGame(userChoice) {
+    userImage.src = `assets/images/${choices[userChoice]}.jpg`;
+    userImage.alt = choices[userChoice];
+
+    let computerChoice = Math.floor(Math.random()*5);
+
+    computerImage.src = `assets/images/${choices[computerChoice]}.jpg`;
+    computerImage.alt = choices[computerChoice];
+
+    let result = checkWinner(choices[computerChoice], choices[userChoice]);
+
+    updateScores(result);
+}
+
+/**Checks for the winner, or for a tie. It accepts two strings */
+function checkWinner(value1, value2) {
+    
+}
